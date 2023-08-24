da53b94fca88b998c655ff820c20df53a1eac1ab
Marvin <MarvAmBass@users.noreply.github.com>
2023-08-24 13:02:07 +0200

    fixed db healthcheck envs - fixed #1

--- scripts/healthchecks.d/database.sh
+++ scripts/healthchecks.d/database.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo 'SHOW TABLES;' | mysql -h db -u zmuser -pzmpass zm 2>/dev/null >/dev/null || exit 1
+echo 'SHOW TABLES;' | mysql -h "$ZM_DB_HOST" -u "$ZM_DB_USER" -p"$ZM_DB_PASS" "$ZM_DB_NAME" 2>/dev/null >/dev/null || exit 1
